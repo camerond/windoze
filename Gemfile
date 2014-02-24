afcaf3631da88f90225dd7a54aba05b6c051d1f9
Cameron Daigle <camerondaigle@gmail.com>
2014-02-24 10:17:32 -0500

    Add deploy

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem "middleman", "~>3.1.2"
 gem "middleman-livereload", "~> 3.1.0"
 
 gem "middleman-syntax"
+gem "middleman-deploy"
 
 gem "bourbon"
 gem "redcarpet"
--- config.rb
+++ config.rb
@@ -3,3 +3,8 @@ activate :syntax
 
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+end
